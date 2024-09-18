060c833c8d9e7ef9ded8753ff06ac3f3f65bda14
taynara veloso <taynarasantos2.3@hotmail.com>
2024-09-17 21:00:22 -0300

    :star: function App

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
+import { CreateGoal } from "./components/create-goal";
+import { EmptyGoals } from "./components/empty-goals";
+import { SummaryGoal } from "./components/summary-goal";
+import { Dialog } from "./components/ui/dialog";
 
 export function App() {
-
   return (
-    <>
-      <h1 className="">Hello Dev</h1>
-    </>
+    <Dialog>
+      <EmptyGoals />
+      <CreateGoal />
+      <SummaryGoal />
+    </Dialog>
   )
 }

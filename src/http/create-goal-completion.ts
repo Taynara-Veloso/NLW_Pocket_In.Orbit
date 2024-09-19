1812faa9386df10aded96cb10ddaa852aab2cd63
taynara veloso <taynarasantos2.3@hotmail.com>
2024-09-19 17:15:58 -0300

    :star: route Create Goal Completion - adjust the route

--- src/http/create-goal-completion.ts
+++ src/http/create-goal-completion.ts
@@ -1,5 +1,5 @@
 export async function createGoalCompletion(goalId: string) {
-  await fetch('http:///localhost:3333/summary-goal', {
+  await fetch('http://localhost:3333/goals-completions', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',

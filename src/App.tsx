cb85779f2fbbe4da8b544cd98d541dc9e6d54dd9
taynara veloso <taynarasantos2.3@hotmail.com>
2024-09-19 16:30:51 -0300

    :star: function App useQuery

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
-import { CreateGoal } from "./components/create-goal";
-import { EmptyGoals } from "./components/empty-goals";
-import { SummaryGoal } from "./components/summary-goal";
 import { Dialog } from "./components/ui/dialog";
+import { useQuery } from "@tanstack/react-query";
+import { EmptyGoals } from "./components/empty-goals";
+import { CreateGoal } from "./components/create-goal";
+import { SummaryGoal } from "./components/summary-goal";
+import { getSummaryGoal } from "./http/get-summary-goal";
 
 export function App() {
+  const { data } = useQuery({
+    queryKey: ['summary'],
+    queryFn: getSummaryGoal,
+    staleTime: 1000 * 60, // 60 seconds
+  })
+
   return (
     <Dialog>
-      <EmptyGoals />
+      {data?.total && data.total > 0 ? <SummaryGoal /> : <EmptyGoals />}
       <CreateGoal />
-      <SummaryGoal />
     </Dialog>
   )
 }
